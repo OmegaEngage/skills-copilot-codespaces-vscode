dde43d7772cdf7c50e9395401284df14aaca78e2
OmegaEngage <138704711+OmegaEngage@users.noreply.github.com>
2023-08-13 21:35:06 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,70 +1,16 @@
 //create web server
-const express = require('express');
+const express = require("express");
 const app = express();
-const port = 3000;
-const bodyParser = require('body-parser');
-
-//create database
-const Datastore = require('nedb');
-const db = new Datastore({ filename: 'comments.db', autoload: true });
-
-//enable parsing of posted data
-app.use(bodyParser.urlencoded({ extended: true }));
-
-//enable parsing of JSON data
-app.use(bodyParser.json());
-
-//enable CORS
-app.use(function(req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
-	next();
-});
-
-//static files
-app.use(express.static('public'));
-
-//start server
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
-
-//REST API
-//GET
-app.get('/api', (request, response) => {
-    //response.send('Hello World!');
-    db.find({}, (err, docs) => {
-        if (err) {
-            console.log('Error: ' + err);
-            response.end();
-            return;
-        }
-        response.json(docs);
-    });
-});
-
-//POST
-app.post('/api', (request, response) => {
-    console.log(request.body);
-    var data = request.body;
-    data.timestamp = Date.now();
-    db.insert(data, (err, newDoc) => {
-        if (err) {
-            console.log('Error: ' + err);
-            response.end();
-            return;
-        }
-        response.json(newDoc);
-    });
-});
-
-//DELETE
-app.delete('/api/:id', (request, response) => {
-    console.log(request.params.id);
-    db.remove({ _id: request.params.id }, {}, (err, numRemoved) => {
-        if (err) {
-            console.log('Error: ' + err);
-            response.end();
-            return;
-        }
-        response.json(numRemoved);
-    });
-});
+//connect to mongodb
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB connected"))
+  .catch(err => console.log(err));
+//create port
+const port = process.env.PORT || 5000;
+//create route
+app.get("/", (req, res) => res.send("Hello World"));
+//listen to port
+app.listen(port, () => console.log(`Server is running on port ${port}`));
