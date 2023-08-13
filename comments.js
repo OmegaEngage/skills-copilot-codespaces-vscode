672864c0c9921a5dbeb6edc9031d7142c7c481d6
OmegaEngage <138704711+OmegaEngage@users.noreply.github.com>
2023-08-13 21:40:16 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,16 +1,68 @@
 //create web server
-const express = require("express");
-const app = express();
-//connect to mongodb
-const mongoose = require("mongoose");
-const db = require("./config/keys").mongoURI;
-mongoose
-  .connect(db)
-  .then(() => console.log("MongoDB connected"))
-  .catch(err => console.log(err));
-//create port
-const port = process.env.PORT || 5000;
-//create route
-app.get("/", (req, res) => res.send("Hello World"));
-//listen to port
-app.listen(port, () => console.log(`Server is running on port ${port}`));
+//import express
+const express = require('express');
+//create router
+const router = express.Router();
+//import comments
+const comments = require('../data/comments');
+
+//get all comments
+router.get('/', (req, res) => {
+    res.json(comments);
+});
+
+//get single comment
+router.get('/:id', (req, res) => {
+    const found = comments.some(comment => comment.id === parseInt(req.params.id));
+    if(found){
+        res.json(comments.filter(comment => comment.id === parseInt(req.params.id)));
+    } else {
+        res.status(400).json({msg: `No comment with the id of ${req.params.id}`});
+    }
+});
+
+//create comment
+router.post('/', (req, res) => {
+    const newComment = {
+        id: comments.length + 1,
+        text: req.body.text,
+        user: req.body.user,
+        post: req.body.post
+    }
+    if(!newComment.text || !newComment.user || !newComment.post){
+        return res.status(400).json({msg: 'Please include a text, user and post'});
+    }
+    comments.push(newComment);
+    res.json(comments);
+});
+
+//update comment
+router.put('/:id', (req, res) => {
+    const found = comments.some(comment => comment.id === parseInt(req.params.id));
+    if(found){
+        const updComment = req.body;
+        comments.forEach(comment => {
+            if(comment.id === parseInt(req.params.id)){
+                comment.text = updComment.text ? updComment.text : comment.text;
+                comment.user = updComment.user ? updComment.user : comment.user;
+                comment.post = updComment.post ? updComment.post : comment.post;
+                res.json({msg: 'Comment updated', comment});
+            }
+        })
+    } else {
+        res.status(400).json({msg: `No comment with the id of ${req.params.id}`});
+    }
+});
+
+//delete comment
+router.delete('/:id', (req, res) => {
+    const found = comments.some(comment => comment.id === parseInt(req.params.id));
+    if(found){
+        res.json({msg: 'Comment deleted', comments: comments.filter(comment => comment.id !== parseInt(req.params.id))});
+    } else {
+        res.status(400).json({msg: `No comment with the id of ${req.params.id}`});
+    }
+});
+
+//export router
+module.exports = router;
