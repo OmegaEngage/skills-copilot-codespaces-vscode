a1398ea9bfe99d6a26fac10df5d3577ae17f4192
OmegaEngage <138704711+OmegaEngage@users.noreply.github.com>
2023-08-13 21:32:12 +0000

    Copilot third commit

--- comments.js
+++ comments.js
@@ -1,78 +1,70 @@
 //create web server
-var express = require('express');
-var app = express();
-var bodyParser = require('body-parser');
-var mongoose = require('mongoose');
-var Comment = require('./model/comment');
-var cors = require('cors');
-var path = require('path');
-var port = 8080;
+const express = require('express');
+const app = express();
+const port = 3000;
+const bodyParser = require('body-parser');
 
-//connect to mongodb
-mongoose.connect('mongodb://localhost:27017/comment');
+//create database
+const Datastore = require('nedb');
+const db = new Datastore({ filename: 'comments.db', autoload: true });
 
-//configure app to use bodyParser()
-//this will let us get the data from a POST
-app.use(bodyParser.urlencoded({extended: true}));
+//enable parsing of posted data
+app.use(bodyParser.urlencoded({ extended: true }));
+
+//enable parsing of JSON data
 app.use(bodyParser.json());
-app.use(cors());
-//set up static file
-app.use(express.static(path.join(__dirname, 'public')));
 
-var router = express.Router();
-
-//middleware to use for all requests
-router.use(function(req, res, next){
-	console.log('Something is happening.');
+//enable CORS
+app.use(function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
 	next();
 });
 
-//test route to make sure everything is working
-router.get('/', function(req, res){
-	res.json({message: 'hooray! welcome to our api!'});
+//static files
+app.use(express.static('public'));
+
+//start server
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+
+//REST API
+//GET
+app.get('/api', (request, response) => {
+    //response.send('Hello World!');
+    db.find({}, (err, docs) => {
+        if (err) {
+            console.log('Error: ' + err);
+            response.end();
+            return;
+        }
+        response.json(docs);
+    });
 });
 
-//on routes that end in /comments
-//----------------------------------------------------
-router.route('/comments')
-	//create a comment (accessed at POST http://localhost:8080/api/comments)
-	.post(function(req, res){
-		var comment = new Comment();
-		comment.content = req.body.content;
-		comment.author = req.body.author;
-		comment.date = req.body.date;
-		comment.save(function(err){
-			if(err){
-				res.send(err);
-			}
-			res.json({message: 'Comment created!'});
-		});
-	})
-	//get all the comments (accessed at GET http://localhost:8080/api/comments)
-	.get(function(req, res){
-		Comment.find(function(err, comments){
-			if(err){
-				res.send(err);
-			}
-			res.json(comments);
-		});
-	});
+//POST
+app.post('/api', (request, response) => {
+    console.log(request.body);
+    var data = request.body;
+    data.timestamp = Date.now();
+    db.insert(data, (err, newDoc) => {
+        if (err) {
+            console.log('Error: ' + err);
+            response.end();
+            return;
+        }
+        response.json(newDoc);
+    });
+});
 
-//on routes that end in /comments/:comment_id
-//----------------------------------------------------
-router.route('/comments/:comment_id')
-	//get the comment with that id (accessed at GET http://localhost:8080/api/comments/:comment_id)
-	.get(function(req, res){
-		Comment.findById(req.params.comment_id, function(err, comment){
-			if(err){
-				res.send(err);
-			}
-			res.json(comment);
-		});
-	})
-	//update the comment with this id (accessed at PUT http://localhost:8080/api/comments/:comment_id)
-	.put(function(req, res){
-		Comment.findById(req.params.comment_id, function(err, comment){
-			if
-
-
+//DELETE
+app.delete('/api/:id', (request, response) => {
+    console.log(request.params.id);
+    db.remove({ _id: request.params.id }, {}, (err, numRemoved) => {
+        if (err) {
+            console.log('Error: ' + err);
+            response.end();
+            return;
+        }
+        response.json(numRemoved);
+    });
+});
